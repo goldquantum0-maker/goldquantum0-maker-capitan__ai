592cfa1c4424701eb494f4a973b854529ad50b4c
goldquantum0-maker <goldquantum0@gmail.com>
2026-06-10 12:43:18 +0000

    Update capitan_api.py

--- capitan_api.py
+++ capitan_api.py
@@ -780,4 +780,4 @@ def ws_get_notes(room_code: str):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",8000))
-    uvicorn.run(app,host="0.0.0.0",port=port)
+    uvicorn.run(app,host="0.0.0.0",port=port) 
