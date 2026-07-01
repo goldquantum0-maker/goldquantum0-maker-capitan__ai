6549b2a7d7e0385e8f470f2766a3b431e65f767c
goldquantum0-maker <goldquantum0@gmail.com>
2026-06-30 20:06:56 -1100

    Update capitan_api.py

--- capitan_api.py
+++ capitan_api.py
@@ -2518,4 +2518,4 @@ def health_check():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port) 
